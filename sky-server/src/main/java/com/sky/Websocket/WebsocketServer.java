60f8f92cc6a63c82e5dd91c255742c6e7892c9ea
sillyboyy <3331553521@qq.com>
2024-11-02 01:40:12 +0800

    完善处理超时订单相关功能

---
+++ sky-server/src/main/java/com/sky/Websocket/WebsocketServer.java
@@ -0,0 +1,49 @@
+package com.sky.Websocket;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@ServerEndpoint("/ws/{sid}")
+@Slf4j
+public class WebsocketServer {
+    private static final Map<String, Session> seesionMap = new ConcurrentHashMap<>();
+
+    private static void sendToAllClient(String message) {
+        for (Session session : seesionMap.values()) {
+            try {
+                session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("sid") String sid) {
+        log.info("客户端+{}+建立连接", sid);
+        seesionMap.put(sid, session);
+    }
+
+    @OnMessage
+    public void onMessage(String message, @PathParam("sid") String sid) {
+        log.info("客户端{}发送消息,{}", sid, message);
+    }
+
+    @OnClose
+    public void onClose(@PathParam("sid") String sid) {
+        log.info("客户端{}断开连接", sid);
+        seesionMap.remove(sid);
+    }
+}
---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,55 @@
+package com.sky.task;
+
+
+import com.sky.constant.MessageConstant;
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void checkOrderStatus() {
+        log.info("定时检查订单状态");
+
+        List<Orders> ordersList = orderMapper.selectByStatusAndOrderTimeLt(
+                Orders.PENDING_PAYMENT,
+                LocalDateTime.now().minusMinutes(15)
+        );
+        if (ordersList != null && !ordersList.isEmpty()) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason(MessageConstant.ORDER_TIME_OUT);
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+    }
+
+    @Scheduled(cron = "0 0 1 * * ? ")
+    public void deliveryOrder() {
+        log.info("处理定时派送订单");
+        List<Orders> ordersList = orderMapper.selectByStatusAndOrderTimeLt(
+                Orders.DELIVERY_IN_PROGRESS,
+                LocalDateTime.now().minusMinutes(60)
+        );
+        if (ordersList != null && !ordersList.isEmpty()) {
+            for (Orders orders : ordersList) {
+                orders.setDeliveryStatus(Orders.COMPLETED);
+
+                orderMapper.update(orders);
+            }
+        }
+    }
+}
